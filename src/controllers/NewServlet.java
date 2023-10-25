65f9812b98cb55c39e67da03c5b3a5ff3d335991
ryan <email>
2023-10-25 17:06:59 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,9 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -39,25 +39,14 @@ public class NewServlet extends HttpServlet {
         Message m = new Message();
 
         // mの各フィールドにデータを代入
-        String title = "taro";
-        m.setTitle(title);
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        String content = "hello";
-        m.setContent(content);
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
-
-        // データベースに保存
-        em.persist(m);
-        em.getTransaction().commit();
-
-        // 自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-        em.close();
-
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
     }
 
 }
